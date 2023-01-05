7113d04f3af4028e3c0140206fa1c27387700b95
Christian <c.espinoza.code@gmail.com>
2023-01-05 10:55:52 -0500

    forEach example added

--- server/1-forEach.ts
+++ server/1-forEach.ts
@@ -1,7 +1,9 @@
-const product = [5,3,4,2,6];
+export const product = [5,3,4,2,6];
 
 // For loop
-
+for(let i=0; i<product.length; i++) {
+  console.log(product[i]);
+}
 
 // For each
 product.forEach((item) => {
